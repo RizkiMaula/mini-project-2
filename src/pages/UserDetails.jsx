b18a25ad474c94c7837765fa45598efda5fe86f0
RizkiMaula <Rmaulana12321@gmail.com>
2024-07-31 20:44:20 +0700

    setup login & register

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -1,3 +1,7 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+
 const UserDetails = () => {
   return <h1>User Details</h1>;
 };
